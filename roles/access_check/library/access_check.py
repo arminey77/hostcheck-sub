7947fad25bfbf20bce2a367f36bbc3053fdd1f4f
Hadi Azaddel <h.azaddel@asax.ir>
2024-03-18 08:37:44 +0000

    Access ssh check issues

--- roles/access_check/library/access_check.py
+++ roles/access_check/library/access_check.py
@@ -34,14 +34,19 @@ def main():
             "dst": {"default": True, "type": "str"},
             "port": {"default": True, "type": "int"},
             "timeout": {"default": False, "type": "int"},
+            "failed_on": {"default": False, "type": "bool"},
         }
     )
     logging.info('Arguments: %s', module.params)
+    print(module.params['failed_on'])
     # Check IP format
     if validate_ip(module.params['dst']):
         logging.debug('Dst has IP format')
         access = telnet(module.params['dst'], module.params['port'], module.params['timeout'])
-        module.exit_json(changed=not access, access=access)
+        if module.params['failed_on']:
+            module.exit_json(failed=not access, changed=not access, access=access)
+        else:
+            module.exit_json(changed=not access, access=access)
     # Domain Format
     else:
         logging.debug('Dst has DOMAIN format')
@@ -53,7 +58,10 @@ def main():
                 logging.debug(ip)
                 access_returns.append(telnet(str(ip), module.params['port'], module.params['timeout']))
             logging.debug(all(access_returns))
-            module.exit_json(changed=not all(access_returns), access=all(access_returns))
+            if module.params['failed_on']:
+                module.exit_json(failed=not all(access_returns), changed=not all(access_returns), access=all(access_returns))
+            else:
+                module.exit_json(changed=not all(access_returns), access=all(access_returns))
         except Exception as e:
             logging.error('Error on DNS resolver %s', e)
             module.fail_json(e)
